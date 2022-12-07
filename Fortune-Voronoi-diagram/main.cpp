c8092e7a5612f21990280b74b8c88e080e13be5b
MinTins <enderator15@gmail.com>
2022-12-07 03:46:19 +0200

    Load fortune-voronoi files

--- Fortune-Voronoi-diagram/main.cpp
+++ Fortune-Voronoi-diagram/main.cpp
@@ -1,142 +1,148 @@
-/*
-K-23 Flakey Roman
-Метод Форчуни. Діаграма Вороного
-*/
+#include <GL/glew.h>
+#include <GL/glut.h>
+#include "Voronoi.h"
+#include "VPoint.h"
 
 
 #include <iostream>
-#include <vector>
-#include <chrono>
-#include <random>
-#include <GL/glew.h>
-#include <glut.h>
-#include "recursiveVoronoi.h"
-#include <windows.h>
-#include <stack>
-#define EPSILON std::numeric_limits<double>::epsilon()
-double w = 600;
-double ww = 800;
+#include<string>
+#include <fstream>
+#include <sstream>
+
+
 void display(void);
 void onEF(int n);
 void reshape(int width, int height);
-std::vector<Vector2> corpos = { Vector2(0.0, w/2),Vector2(w / 2, 0.0),Vector2(w, w / 2) ,Vector2(w / 2, w) };
-DiscreteGlobalGrid discreteGlobalGrid;
 
-void drawPoints()
+vor::Voronoi * v;
+vor::Vertices * ver; vor::Vertices * dir; vor::Edges * edg; 
+double w = 10000;
+const int POINTS_COUNT = 100;
+const float SPEED_DIVIDER = 500; 
+
+int main(int argc, char** argv)
 {
-	std::vector<Vector2> points = discreteGlobalGrid.points;
-	glBegin(GL_QUADS);
-	for (std::size_t i = 0; i < points.size(); ++i)
-	{
-		Vector2 p = points[i];
-		glVertex2f(-1 + 2 * (p.x + (ww - w) / 2) / ww - 0.01, -1 + 2 * (p.y + (ww - w) / 2) / ww - 0.01);
-		glVertex2f(-1 + 2 * (p.x + (ww - w) / 2) / ww + 0.01, -1 + 2 * (p.y + (ww - w) / 2) / ww - 0.01);
-		glVertex2f(-1 + 2 * (p.x + (ww - w) / 2) / ww + 0.01, -1 + 2 * (p.y + (ww - w) / 2) / ww + 0.01);
-		glVertex2f(-1 + 2 * (p.x + (ww - w) / 2) / ww - 0.01, -1 + 2 * (p.y + (ww - w) / 2) / ww + 0.01);
-	}
-	glEnd();
-}
-void drawOtherPoints()
-{
-	std::vector<Vector2> points = discreteGlobalGrid.otherTestPoints;
-	glBegin(GL_QUADS);
-	for (std::size_t i = 0; i < points.size(); ++i)
-	{
-		Vector2 p = points[i];
-		glVertex2f(-1 + 2 * (p.x + (ww - w) / 2) / ww - 0.01, -1 + 2 * (p.y + (ww - w) / 2) / ww - 0.01);
-		glVertex2f(-1 + 2 * (p.x + (ww - w) / 2) / ww + 0.01, -1 + 2 * (p.y + (ww - w) / 2) / ww - 0.01);
-		glVertex2f(-1 + 2 * (p.x + (ww - w) / 2) / ww + 0.01, -1 + 2 * (p.y + (ww - w) / 2) / ww + 0.01);
-		glVertex2f(-1 + 2 * (p.x + (ww - w) / 2) / ww - 0.01, -1 + 2 * (p.y + (ww - w) / 2) / ww + 0.01);
-	}
-	glEnd();
-}
-void drawDiagram()
-{
-	std::vector<Edge> edges = discreteGlobalGrid.edges;
-	int dcount = edges.size();
-	for (size_t i = 0; i < dcount; i++)
-	{
-		Edge e = edges[i];
-		std::vector<Vector2> origin = e.origin;
-		std::vector<Vector2> destination = e.destination;
-		int vcount = origin.size();
-		for (size_t j = 0; j < vcount; j++)
+	using namespace vor;
+
+	v = new Voronoi();
+	ver = new Vertices();
+	dir = new Vertices();
+
+	int mode;
+
+	std::cout << "K-23 Flakey Roman\nFortune-Voronoi Diagram\n\nChoose method:\n	1) random point\n	2) own points" << std::endl;
+	std::cin >> mode;
+	/**/
+
+	if (mode == 1) {
+
+		srand(time(NULL));
+
+		for (int i = 0; i < POINTS_COUNT; i++)
 		{
-			glBegin(GL_LINES);
-			glVertex2f(-1 + 2 * (origin[j].x + (ww - w) / 2) / ww, -1 + 2 * (origin[j].y + (ww - w) / 2) / ww);
-			glVertex2f(-1 + 2 * (destination[j].x + (ww - w) / 2) / ww, -1 + 2 * (destination[j].y + (ww - w) / 2) / ww);
-			glEnd();
+			ver->push_back(
+				new VPoint(
+					w * (double)rand() / (double)RAND_MAX,
+					w * (double)rand() / (double)RAND_MAX
+				)
+			);
+
+			dir->push_back(
+				new VPoint(
+					(double)rand() / (double)RAND_MAX - 0.5,
+					(double)rand() / (double)RAND_MAX - 0.5
+				)
+			);
 		}
 	}
+	else if (mode == 2) {
+		std::string filename;
+		std::cout << "Input name of your file with points:" << std::endl;
+		std::cin >> filename;
+
+		std::ifstream PointFile(filename);
+
+		std::string s;
+		while(std::getline(PointFile, s)) {
+			std::istringstream sin(s);
+			double x,y;
+			sin >> x;
+			sin >> y;
+
+			ver->push_back(
+				new VPoint(x,y)
+			);
+
+			dir->push_back(
+				new VPoint(x,y)
+			);
+		
+			}
+		}
+	
+	glutInit(&argc, argv); 	glutInitDisplayMode (GLUT_SINGLE); 	glutInitWindowSize (800, 800); 	glutInitWindowPosition (100, 100); 	glutCreateWindow ("Lab 03"); 
+		glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	glEnable(GL_LINE_SMOOTH);
+	glHint(GL_LINE_SMOOTH, GL_NICEST);
+	glEnable(GL_POINT_SMOOTH);
+	glHint(GL_POINT_SMOOTH, GL_NICEST);
+	
+	glutTimerFunc(100, onEF, 0); 	glutDisplayFunc(display); 	glutReshapeFunc(reshape); 	glutMainLoop(); 	return 0;
 }
 
 void drawVoronoi()
 {
-	drawDiagram();
+	vor::Vertices::iterator j = dir->begin();
+	for(vor::Vertices::iterator i = ver->begin(); i != ver->end(); ++i)
+	{
+		(*i)->x += (*j)->x * w/SPEED_DIVIDER;
+		(*i)->y += (*j)->y * w/SPEED_DIVIDER;
+		if( (*i)->x > w || (*i)->x < 0) (*j)->x *= -1;
+		if( (*i)->y > w || (*i)->y < 0) (*j)->y *= -1;
+		++j;
+	}
+	
+	int c = 10;
+
+	edg = v->GetEdges(ver, w, w);  
+	for(vor::Vertices::iterator i = ver->begin(); i!= ver->end(); ++i)
+	{
+		glBegin(GL_QUADS);
+		glColor3b(c*2, c*3, c*4);
+		glVertex2f( -1+2*(*i)->x/w -0.01,  -1+2*(*i)->y/w - 0.00);
+		glVertex2f( -1+2*(*i)->x/w +0.00,  -1+2*(*i)->y/w - 0.01);
+		glVertex2f( -1+2*(*i)->x/w +0.01,  -1+2*(*i)->y/w + 0.00);
+		glVertex2f( -1+2*(*i)->x/w -0.00,  -1+2*(*i)->y/w + 0.01);
+		glEnd();
+		c += 2;
+	}
+	
+	for(vor::Edges::iterator i = edg->begin(); i!= edg->end(); ++i)
+	{
+		glBegin(GL_LINES);
+		glColor3b(120, 140, 160); 		glVertex2f(-1+2*(*i)->start->x/w, -1+2*(*i)->start->y/w);
+		glColor3b(20, 40, 60); 		glVertex2f( -1+2*(*i)->end->x/w, -1+2*(*i)->end->y/w);
+		glEnd();
+	}
 }
 
-void display(void)
+void display (void) 
 {
-	std::cout << "display\n";
 	glLoadIdentity();
-	glTranslatef(0.0f, 0.0f, -5.0f);
-
+	glTranslatef(0.0f, 0.0f, -5.0f); 
 	glFlush();
 }
+
+
 void onEF(int n)
 {
-
-	glutTimerFunc(20, onEF, 0);
-	glClear(GL_COLOR_BUFFER_BIT);//Clear the screen
-	glClearColor(0.0f, 0.0f, 0.2f, 1.0f);
-
+	glutTimerFunc(50, onEF, 0);
+	glClear(GL_COLOR_BUFFER_BIT); 	glClearColor(0.0f, 0.0f, 0.0f, 1.0f); 	
 	drawVoronoi();
 	glutSwapBuffers();
 }
 
-void reshape(int width, int height)
+void reshape (int width, int height) 
 {
-
-	glViewport(0, 0, (GLsizei)width, (GLsizei)height);
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	gluPerspective(22.5, (GLfloat)width / (GLfloat)height, 1.0, 100.0);
-	glMatrixMode(GL_MODELVIEW);
-}
-
-double get_angle(double x1, double y1, double x2, double y2, double x3, double y3)
-{
-	double theta = atan2(x1 - x3, y1 - y3) - atan2(x2 - x3, y2 - y3);
-	if (theta > std::_Pi)
-		theta -= 2 * std::_Pi;
-	if (theta < -std::_Pi)
-		theta += 2 * std::_Pi;
-
-	theta = abs(theta * 180.0 / std::_Pi);
-	return theta;
-}
-
-int main(int argc, char **argv)
-{
-	double a = get_angle(373.5030592, 0, 294.2053188, 62.68338888, 338.1511887, 0.750755333);
-	double b = get_angle(294.2053188,62.68338888,283.9666317,40.06746694,338.1511887,0.750755333);
-	double c = get_angle(283.9666317,40.06746694,282.3138898,0,338.1511887,0.750755333);
-	double d = get_angle(282.3138898,0,373.5030592,0,338.1511887,0.750755333);
-	recursiveVoronoi *rv = new recursiveVoronoi;
-	rv->begin(corpos, 1);
-	discreteGlobalGrid = rv->getDiscreteGlobalGrid();
-
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_DOUBLE);
-	glutInitWindowSize(600, 600);
-	glutInitWindowPosition(100, 100);
-	glutCreateWindow("first OpenGL Window");
-
-	glutTimerFunc(100, onEF, 0);
-	glutDisplayFunc(display);
-
-	glutReshapeFunc(reshape);
-
-	glutMainLoop();
-	return 0;
-}
+	glViewport(0, 0, (GLsizei)width, (GLsizei)height); 	glMatrixMode(GL_PROJECTION); 	glLoadIdentity(); 	gluPerspective(22.5, (GLfloat)width / (GLfloat)height, 1.0, 100.0); 	glMatrixMode(GL_MODELVIEW); }
